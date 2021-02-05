7ad4a2ce24119ed8d7b07bc3953d6e6c3fac2db8
Oscar Fonseca <ogfonseca9@gmail.com>
2021-02-05 00:31:51 -0500

    Add function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 def index():
     return "Hello, world!"
+
+def add(x, y):
+    return x + y
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,12 @@
-from src.main import index
+from src.main import index, add
 
 def test_index():
     assert index() == "Hello, world!"
+
+@pytest.mark.parametrize('x, y, result', [
+    (10, 10, 20),
+    (5, 5, 10),
+    (6, 6, 12)
+])
+def test_add(x, y, result):
+    assert add(x, y) == result
