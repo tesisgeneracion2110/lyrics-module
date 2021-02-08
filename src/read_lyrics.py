f6ad20f833096b3eb2ac4f42ad26a16ce388e7ae
Oscar Fonseca <ogfonseca9@gmail.com>
2021-02-08 17:45:46 -0500

    first test of file read

--- src/read_lyrics.py
+++ src/read_lyrics.py
@@ -1,3 +1,5 @@
+import re
+
 def delete_empty_lines(content):
     lines = content.split("\n")
     non_empty_lines = [line for line in lines if line.strip() != ""]
@@ -7,20 +9,27 @@ def delete_empty_lines(content):
     return content
 
 
-def adapt_lyrics(content):
+def delete_square_brackets_lines(content):
     lines = content.split("\n")
-    non_empty_lines = [line for line in lines if "[" not in line]
+    non_square_brackets_lines = [line for line in lines if "[" not in line]
     content = ""
-    print(non_empty_lines)
-    for line in non_empty_lines:
+    for line in non_square_brackets_lines:
         content += line + "\n"
     return content
 
 
-file = open("lyrics.txt", encoding="utf8")
+def delete_all_brackets(content):
+    result = re.sub(r"\([^()]*\)", "", content)
+    return result
+
+
+file = open("../lyrics.txt", "r", encoding="utf8")
 file_content = file.read()
 lyrics = delete_empty_lines(file_content)
-print(lyrics)
-adapt_lyrics(lyrics)
-
-# print(file_content)
+lyrics = delete_square_brackets_lines(lyrics)
+lyrics_one = lyrics.replace("\\", "")
+lyrics_two = delete_all_brackets(lyrics)
+lyrics_three = delete_all_brackets(file_content)
+f = open("lyrics1.txt", "w", encoding="utf8")
+f.write(lyrics_three)
+f.close()
