8f7a31045348dc5e2a67c9d04c03b919d6085f56
Oscar Fonseca <ogfonseca9@gmail.com>
2021-02-08 18:41:54 -0500

    Add lyrics2 and array

--- src/read_lyrics.py
+++ src/read_lyrics.py
@@ -22,18 +22,26 @@ def delete_all_brackets(content):
     result = re.sub(r"\([^()]*\)", "", content)
     return result
 
+def to_array(lyrics):
+    array = lyrics.split("\n\n")
+    return array
 
 file = open("lyrics.txt", "r", encoding="utf8")
 file_content = file.read()
 lyrics = delete_empty_lines(file_content)
 lyrics = delete_square_brackets_lines(lyrics)
 lyrics_one = lyrics.replace("\\", "")
-lyrics_two = delete_all_brackets(lyrics)
+lyrics_two = delete_all_brackets(lyrics_one)
 lyrics_three = delete_all_brackets(file_content)
 lyrics_three = delete_square_brackets_lines(lyrics_three)
 f = open("lyrics1.txt", "w", encoding="utf8")
 f.write(lyrics_one)
 f.close()
+f = open("lyrics2.txt", "w", encoding="utf8")
+f.write(lyrics_two)
+f.close()
 f = open("lyrics3.txt", "w", encoding="utf8")
 f.write(lyrics_three)
 f.close()
+
+lyrics_array = to_array(lyrics_one)
